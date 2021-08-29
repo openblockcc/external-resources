300b05b6712db5984946840978eebf11a4482503
ArthurZheng <869705086@qq.com>
2021-08-29 16:46:38 +0800

    adjust the message of 8-Digit Display

--- extensions/arduino/display/eightDigitDisplay/msg.js
+++ extensions/arduino/display/eightDigitDisplay/msg.js
@@ -4,16 +4,16 @@ function addMsg (Blockly) {
     Object.assign(Blockly.ScratchMsgs.locales.en, {
         EIGHTDIGITDISPLAY_CATEGORY: '8-Digit Display',
         EIGHTDIGITDISPLAY_INIT: 'init 8-digit display pin DIN %1 CS %2 CLK %3',
-        EIGHTDIGITDISPLAY_DISPLAYNUMBER: 'display number %1 decimal %2',
-        EIGHTDIGITDISPLAY_SETBRIGHTNESS: 'set brightness %1',
-        EIGHTDIGITDISPLAY_CLEAR: 'clear'
+        EIGHTDIGITDISPLAY_DISPLAYNUMBER: '8-digit display show number %1 decimal %2',
+        EIGHTDIGITDISPLAY_SETBRIGHTNESS: 'set 8-digit display brightness %1',
+        EIGHTDIGITDISPLAY_CLEAR: 'clear 8-digit display'
     });
     Object.assign(Blockly.ScratchMsgs.locales['zh-cn'], {
         EIGHTDIGITDISPLAY_CATEGORY: '8位数码管',
         EIGHTDIGITDISPLAY_INIT: '初始化 8位数码管引脚 DIN %1 CS %2 CLK %3',
-        EIGHTDIGITDISPLAY_DISPLAYNUMBER: '显示数字 %1 保留小数点后 %2 位',
-        EIGHTDIGITDISPLAY_SETBRIGHTNESS: '设置亮度 %1',
-        EIGHTDIGITDISPLAY_CLEAR: '清空'
+        EIGHTDIGITDISPLAY_DISPLAYNUMBER: '8位数码管显示数字 %1 保留小数点后 %2 位',
+        EIGHTDIGITDISPLAY_SETBRIGHTNESS: '设置8位数码管亮度 %1',
+        EIGHTDIGITDISPLAY_CLEAR: '清空8位数码管'
     });
     return Blockly;
 }
